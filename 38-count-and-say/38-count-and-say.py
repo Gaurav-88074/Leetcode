481c9c3c2faca6f6ace4550d2f907121eb254ad3
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-03 10:00:18 +0530

    Time: 43 ms (66.02%), Space: 13.4 MB (86.48%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def compute(self,arr):
+        if len(arr)==1 and arr[0]==1:
+            return [1,1]
+        res = []
+        c=0
+        pre = arr[0]
+        for i in arr:
+            if i==pre:
+                c+=1
+            else:
+                res.append(c)
+                res.append(pre)
+                pre=i
+                c=1
+        res.append(c)
+        res.append(pre)
+        return res
+    def countAndSay(self, n):
+        if n==1:
+            return "1"
+        
+        r = [1]
+        for i in range(n-1):
+            r = self.compute(r)
+            # print(r)
+        return "".join(map(str,r))
+        
