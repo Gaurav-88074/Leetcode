c1ea0c956f8efa9fc233484dc22b7cac035e5e01
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-04 19:07:37 +0530

    Time: 4100 ms (20.00%), Space: 707.6 MB (40.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -9,7 +9,8 @@ class Solution:
             if k==0 and s!=e:
                 return 0
             
-            return sum([compute(s+1,e,k-1),compute(s-1,e,k-1)])
+            # return sum([compute(s+1,e,k-1),compute(s-1,e,k-1)])
+            return compute(s+1,e,k-1) + compute(s-1,e,k-1)
         res = compute(s,e,k)
         # print(res)
         mod = 10**9 + 7
