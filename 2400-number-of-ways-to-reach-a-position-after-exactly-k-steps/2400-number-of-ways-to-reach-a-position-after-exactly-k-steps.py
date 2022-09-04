c6ed33fa3c9a8c286ef7c87c5f345abcc8df959d
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-04 09:34:21 +0530

    Time: 4390 ms (20.00%), Space: 708.6 MB (20.00%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfWays(self, s: int, e: int, k: int) -> int:
+        # res = 0
+        @lru_cache(None)
+        def compute(s,e,k):
+            if k==0 and s==e:
+                # print("yes")
+                return 1
+            if k==0 and s!=e:
+                return 0
+            
+            return sum([compute(s+1,e,k-1),compute(s-1,e,k-1)])
+        res = compute(s,e,k)
+        # print(res)
+        mod = 10**9 + 7
+        return res%mod
+                
