4d5090367dc51c81e2a1d8fdb07c4aa26d9d0ca2
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-06 15:52:43 +0530

    Time: 293 ms (47.04%), Space: 14.9 MB (55.70%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -20,17 +20,13 @@ class Solution:
             else:
                 v.append(v[-1]-1)
             #-------------
+            
+            #-------------
+        # print(v)
+        for i in range(len(arr)):
             if v[i]<0:
                 res+=arr[i]
                 continue
-            if arr[i]!=v[i]:
-                res+=abs(arr[i]-v[i])
-            #-------------
-        # print(v)
-        # for i in range(len(arr)):
-        #     if v[i]<0:
-        #         res+=arr[i]
-        #         continue
-        #     if arr[i]!=v[i]:res+=abs(arr[i]-v[i])
+            if arr[i]!=v[i]:res+=abs(arr[i]-v[i])
                 
         return res 
