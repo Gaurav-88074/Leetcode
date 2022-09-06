ab6a5ac9bf118c31b07207647f06f1f11165b5bc
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-06 16:01:30 +0530

    Time: 517 ms (6.67%), Space: 14.2 MB (58.62%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -9,8 +9,16 @@ class Solution:
             
             subArray = nums[left :right+1]
             subArray.sort()
+            
+            conti = False
             for i in range(1,len(subArray)):
                 st.add(subArray[i]-subArray[i-1])
-            
+                if len(st)>1:
+                    res.append(False)
+                    conti = True
+                    break
+                    
+            if conti:continue
+                
             res.append(len(st)==1)
         return res
