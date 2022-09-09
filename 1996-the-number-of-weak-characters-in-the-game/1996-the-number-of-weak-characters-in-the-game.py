3f63911148ab23cd8f346de050e94e167b891452
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-09 12:42:34 +0530

    Time: 3914 ms (29.64%), Space: 67.6 MB (49.26%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -3,7 +3,8 @@ class Solution:
         def comp(a,b):
             if a[0]==b[0] : return b[1]-a[1]
             return a[0]-b[0]
-        properties.sort(key = cmp_to_key(comp))
+        # properties.sort(key = cmp_to_key(comp))
+        properties.sort(key = lambda x : (x[0],-x[1]))
         
         res = 0
         # print(properties)
