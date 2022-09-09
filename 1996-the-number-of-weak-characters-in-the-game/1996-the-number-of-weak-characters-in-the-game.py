308de6cca2868fda490816713c69e064974b4c89
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-09 12:31:23 +0530

    Time: 4976 ms (6.68%), Space: 67.7 MB (49.26%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        def comp(a,b):
+            if a[0]==b[0] : return b[1]-a[1]
+            return a[0]-b[0]
+        properties.sort(key = cmp_to_key(comp))
+        
+        res = 0
+        # print(properties)
+        #------------------------------------------
+        arr = [0]*len(properties)
+        
+        stack = [-1]
+        for i in range(len(properties)-1,-1,-1):
+            while stack[-1]!=-1 and stack[-1]<=properties[i][1]:
+                stack.pop()
+            arr[i] = stack[-1]
+    
+            stack.append(properties[i][1])
+        #----------------------------------------------
+#         vrr = [0]*len(properties)
+        
+#         stack = [-1]
+#         for i in range(len(properties)-1,-1,-1):
+#             while stack[-1]!=-1 and stack[-1]<=properties[i][0]:
+#                 stack.pop()
+#             vrr[i] = stack[-1]
+    
+#             stack.append(properties[i][0])
+        #------------------------------------------------
+        # print(vrr)
+        # print(arr)
+        for i in range(len(properties)):
+            if arr[i]!=-1 :
+                res+=1
+        return res
