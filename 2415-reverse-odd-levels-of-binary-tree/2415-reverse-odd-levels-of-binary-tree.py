a984f2cc2785b9e5bc0310da721423cde426d482
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-18 14:57:26 +0530

    Time: 973 ms (90.91%), Space: 20.6 MB (100.00%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -12,24 +12,28 @@ class Solution:
             if root==None:
                 return
             
-            d[level].append(root.val)
+            d[level].append(root)
             compute(root.left,level+1)
             compute(root.right,level+1)
         compute(root,0)
-        # print(d)
-        # for i in d:
-        #     if (i&1)==1:
-        #         d[i] = list(reversed(d[i]))
-        # print(d)
-        def visit(root,level):
-            if root==None:
-                return
+        for i in d:
             
-            if (level%2)==1:
-                root.val = d[level].pop()
-            visit(root.left,level+1)
-            visit(root.right,level+1)
-        # print(d)
-        visit(root,0)
+            if i%2!=0:
+                arr = d[i]
+                values = [arr[-i].val  for i in range(1,len(arr)+1)]
+                for i in range(len(arr)):
+                    arr[i].val = values[i]
         return root
+#         def visit(root,level):
+#             if root==None:
+#                 return
+            
+#             if (level%2)==1:
+#                 root.val = d[level].pop()
+#             visit(root.left,level+1)
+#             visit(root.right,level+1)
+#         # print(d)
+#         visit(root,0)
+        
+        
                 
