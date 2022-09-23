474f8d079b1d107f8d5be96957db363f8c56daf0
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-09-23 07:28:42 +0530

    Time: 1658 ms (79.53%), Space: 13.8 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -5,9 +5,8 @@ class Solution:
         for x in range(1, n+1):
             output <<= x.bit_length()
             output |= x
-            
-            # output %= 10**9 + 7
+        
             output%=mod
-        return output%mod
+        return output
         # return int(''.join(format(x,"b") for x in range(1, n+1)), 2) % 1_000_000_007
         
