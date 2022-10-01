0a5942469813ab05191a59b8a383601669c1d83d
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-01 14:01:05 +0530

    Time: 84 ms (5.21%), Space: 14.2 MB (19.74%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0]=="0":return 0
+        
+        @lru_cache(None)
+        def compute(index,value):
+            if index==len(s): 
+                # print(value)
+                return 1
+            
+            newValue = value*10 + int(s[index])
+            
+            a = 0
+            b = 0
+            c = 0
+            if newValue<=26:
+                a = compute(index+1,newValue)
+            if int(s[index])!=0 and newValue!=int(s[index]):
+                b = compute(index+1,int(s[index]))
+            if int(s[index])==0  and newValue>26:
+                return 0
+            # if int(s[index])==0:
+            #     c = compute(index+1,0)
+                            
+            return a+b+c
+        return compute(0,0)
