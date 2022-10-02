1069f9e6a82c9ed07915b1b90836095f73b8c490
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-02 09:49:31 +0530

    Time: 16 ms (100.00%), Space: 13.2 MB (100.00%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        res= 0
+        for i in range(1,min(a,b)+1):
+            if a%i==0 and b%i==0:
+                res+=1
+        return res
+        
