c1037d1f5d671648aeae685ea322b44694c7a438
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-03 15:17:57 +0530

    Time: 12 ms (71.63%), Space: 97.2 MB (30.77%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int c = 0;
+        int totalSum = 0;
+        int res  =0;
+        int m = 0;
+        
+        char pre = colors.charAt(0);
+        c =1;
+        m = neededTime[0];
+        // totalSum = m;
+        int i;
+        for(i =1; i <colors.length() ; i++){
+            
+            if(colors.charAt(i)==pre){
+                c+=1;
+                m = Math.max(m,neededTime[i]);
+                totalSum+=neededTime[i-1];
+            }
+            else{
+                if(c>1){
+                    totalSum+=neededTime[i-1];
+                    totalSum-= m;
+                    // System.out.println("At index :"+i+" "+m);
+                }
+                c=1;
+                pre = colors.charAt(i);
+                m = neededTime[i];
+            }
+            
+        }
+        if(c>1){
+            totalSum+=neededTime[i-1];
+            totalSum-= m;
+            // System.out.println("At index :"+i+" "+m);         
+        }
+        return totalSum;
+    }
+}
