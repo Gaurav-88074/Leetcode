a1494cfa2b74938fd319af22b0a34090def10b9a
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-03 23:31:22 +0530

    Time: 3582 ms (54.74%), Space: 14.8 MB (44.62%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
@@ -1,33 +1,14 @@
 class Solution(object):
-    def lenLongestFibSubseq(self, arr):
-        #-----------------------------------------
-        n = len(arr)
-        res = 0
-        s = set(arr)
-        #------------------------------------------
-        def binarySearch(array,key,start,end):
-            if(start>end) : return -1
-            mid = (start+end)//2
-            if key==array[mid] : return mid
-            elif key<array[mid] : return binarySearch(array,key,start,mid-1);
-            else : return binarySearch(array,key,mid+1,end)
-        #----------------------------------------
-        def compute(array,first,second,s):
-            key = first + second
-            
-            if key in s:
-                third = second + first
-                return 1 + compute(array,second,third,s)
-            else:
-                return 0
-        #----------------------------------------
-        for i in range(n):
-            for j in range(i+1,n):
-                first = arr[i]
-                second= arr[j]
-                # self.v=[first,second]
-                value = compute(arr,first,second,s)
-                if value==0:continue
-                res = max(res,value+2)
-        #-----------------------------------------
-        return res
+    def lenLongestFibSubseq(self, A):
+        index = {x: i for i, x in enumerate(A)}
+        longest = collections.defaultdict(lambda: 2)
+
+        ans = 0
+        for k, z in enumerate(A):
+            for j in range(k):
+                i = index.get(z - A[j], None)
+                if i is not None and i < j:
+                    cand = longest[j, k] = longest[i, j] + 1
+                    ans = max(ans, cand)
+
+        return ans if ans >= 3 else 0
