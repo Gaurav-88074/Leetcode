1d391b15c98b97b96d2c2b3993e172407f6dc8e8
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-06 12:05:18 +0530

    Time: 6098 ms (22.81%), Space: 26.9 MB (5.92%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,27 +1,12 @@
-class Solution(object):
-    def rotateTheBox(self, box):
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         def fix(a,b,row):
-            # sub = row[a:b+1]
-            # sub.sort(reverse = True)
-            # for i in sub:
-            #     row[a]=i
-            #     a+=1
-            hashh = 0
-            dot = 0
-            for i in range(a,b+1):
-                if row[i]=="#": 
-                    hashh+=1
-                else: 
-                    dot+=1
-            while dot!=0:
-                row[a]="."
-                dot-=1
+            sub = row[a:b+1]
+            sub.sort(reverse = True)
+            for i in sub:
+                row[a]=i
                 a+=1
-            while hashh!=0:
-                row[a]="#"
-                hashh-=1
-                a+=1
-                
+            
         for row in box:
             start = 0
             for i in range(len(row)):
@@ -41,4 +26,3 @@ class Solution(object):
         
         
         return res
-        
