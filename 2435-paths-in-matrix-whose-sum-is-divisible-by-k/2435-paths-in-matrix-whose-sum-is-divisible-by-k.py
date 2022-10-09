26d1387001e2dc69967d4831bf35437b65e4aeec
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-09 09:41:29 +0530

    Time: 5602 ms (100.00%), Space: 156.1 MB (100.00%) - LeetHub

---
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numberOfPaths(self, grid, k):
+        mod = 10**9 + 7
+        m  =len(grid)
+        n = len(grid[0])
+        dp =[[None]*n for i in range(m)]
+        for i in range(m):
+            for j in range(n):
+                dp[i][j] = [None]*k
+        
+        # @lru_cache(None)
+        def compute(x,y,s):
+            if x==len(grid)-1 and y==len(grid[0])-1:
+                s+=grid[x][y]
+                if s%k==0:
+                    return 1
+                return 0
+            if x==len(grid) or y==len(grid[0]):
+                return 0
+            
+            s+=grid[x][y]
+            s%=k
+            if dp[x][y][s]!=None : return dp[x][y][s]
+            
+            right = compute(x,y+1,s)
+            down = compute(x+1,y,s)
+            
+            res  = right + down
+            dp[x][y][s] = res%mod
+            return res
+        
+        return compute(0,0,0)%mod
