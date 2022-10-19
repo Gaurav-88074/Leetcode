9d5756dfc52c8f59911c6542aeaad6e46f87b341
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-19 21:09:23 +0530

    Time: 149 ms (31.19%), Space: 93.6 MB (23.80%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -0,0 +1,50 @@
+class node{
+    public node[] next ;
+    public boolean isEnd = false;
+    public node(){
+        this.next = new node[128];
+    }
+}
+class Trie {
+    private node root = null;
+    public Trie() {
+        this.root = new node();
+    }
+    public boolean insert(node root,String word,int index) {
+        if(index==word.length()){
+            root.isEnd = true;
+            return true;
+        }
+        int ch = word.charAt(index);
+        if( root.isEnd==true && 
+            (
+                // index<word.length() 
+                // && 
+                word.charAt(index)=='/'
+            ) 
+            // || 
+            // index+1==word.length() 
+          ){
+            return false;
+        }
+        if(root.next[ch]==null) root.next[ch] = new node();
+        return insert(root.next[ch],word,index+1);
+    }
+    public boolean insert(String word) {
+        return insert(root,word,0);
+    }
+}
+
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        var trie = new Trie();
+        List<String> res = new ArrayList<>();
+        for(String s : folder){
+            if(trie.insert(s)){
+                res.add(s);
+            }
+        }
+        return res;
+    }
+}
