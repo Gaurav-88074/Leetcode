5805693e1a1ceb92b5a6c3715844b8359847ea4f
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-21 06:29:00 +0530

    Time: 56 ms (42.26%), Space: 13.9 MB (23.24%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split(" ")
+        if len(s)!=len(pattern):return False
+        d = dict()
+        check =set()
+        for i in range(len(pattern)):
+            v = pattern[i]
+            if v in d:
+                if d[v]!=s[i]:
+                    return False
+            else:
+                if s[i] in check:return False
+                d[v] = s[i]
+                check.add(s[i])
+                
+    
+                
+        return True
