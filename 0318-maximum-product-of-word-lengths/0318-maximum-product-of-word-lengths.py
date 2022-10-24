2006c0b3f82a474afc0dd89413f48d44720227ca
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-24 06:16:19 +0530

    Time: 6632 ms (5.33%), Space: 17.5 MB (38.05%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        res = 0
+        mask = [set(list(ww)) for ww in words]
+        for i in range(n):
+            a = words[i]
+            for j in range(n):
+                if i!=j:
+                    b = words[j]
+                    sa = mask[i]
+                    sb = mask[j]
+                    if len(sa.intersection(sb))==0:
+                        res = max(len(a)*len(b),res)
+        return res
+                    
