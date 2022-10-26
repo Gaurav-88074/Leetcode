502307d255672c8370ae2c1c84ef077ea6bbf519
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-26 10:36:40 +0530

    Time: 2511 ms (30.62%), Space: 33.1 MB (73.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -4,13 +4,13 @@ class Solution:
         pre = 0
         for i in range(len(nums)):
             pre+=nums[i]
-            
+
             mod = pre%k
             
             if i>=1 and mod==0 :
                 return True
             if mod in d and i-d[mod]>=2:
                 return True
-            if mod not in d :
+            elif mod not in d :
                 d[mod] = i
         return False
