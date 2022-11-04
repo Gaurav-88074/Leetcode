fd44cbc195565d57a13ba169d0914ac33b957755
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-11-04 16:27:19 +0530

    Time: 32 ms (88.89%), Space: 13 MB (100.00%) - LeetHub

--- 0479-largest-palindrome-product/0479-largest-palindrome-product.py
+++ 0479-largest-palindrome-product/0479-largest-palindrome-product.py
@@ -1,4 +1,5 @@
-class Solution:
-    def largestPalindrome(self, n: int) -> int:
+class Solution(object):
+    def largestPalindrome(self, n):
         arr =[9,987,123,597,677,1218,877,475]
         return arr[n-1]
+        
