5e34067c9be38e2054cfa36f3175ef796724cf40
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-11-07 19:15:00 +0530

    Time: 47 ms (72.85%), Space: 13.9 MB (11.49%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        arr = []
+        sign = 1
+        if num<0:
+            sign=-1
+            num*=-1
+        while num>=7:
+            rem = num%7
+            num//=7
+            arr.append(rem)
+        arr.append(num)
+        arr.reverse()
+        # print(arr)
+        res = list(map(str,arr))
+        res = "".join(res)
+        if sign==-1:
+            return "-"+res
+        return res
+            
