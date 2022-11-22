b4dd892a8de4f82f758cb0078820e9f09d3840c0
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-11-22 06:22:21 +0530

    Time: 206 ms (21.09%), Space: 43.8 MB (32.70%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minCoin(int[] coins,int amount,int[] dp){
+        
+        if(amount<0){
+            return Integer.MAX_VALUE;
+            // return -1;
+        }
+        if(amount==0){
+            return 0;
+        }
+        if(dp[amount]!=-1){
+            return dp[amount];
+        }
+        int min = Integer.MAX_VALUE;
+        int value;
+        for(int i = 0 ; i<coins.length ; i++){
+            value = minCoin(coins,amount-coins[i],dp);
+            if(amount-coins[i]>-1)
+                dp[amount-coins[i]] = value;
+            if(value!=Integer.MAX_VALUE){
+                min = Math.min(min,1 + value);
+            }
+        }
+        dp[amount] = min;
+        return dp[amount];
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,-1);
+        int res =  minCoin(coins,amount,dp);
+        if(res==Integer.MAX_VALUE)
+            return -1;
+        return res;
+    }
+    public int numSquares(int n) {
+
+        int v=1;
+        List<Integer> value = new ArrayList<>();
+        while(v*v<=n){
+            value.add(v*v);
+            v+=1;
+        }
+        int[] values = value.stream().mapToInt(i -> i).toArray();
+         // System.out.println(Arrays.toString(values));
+        
+        return coinChange(values,n);
+    }
+}
