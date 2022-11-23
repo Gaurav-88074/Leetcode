c99b2706d65d695a411b9be37bb4e6e04cbc30d1
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-11-23 11:27:29 +0530

    Time: 2420 ms (68.39%), Space: 25.5 MB (29.21%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,47 @@
+class Solution:
+    def countPairs(self, d: List[int]) -> int:
+        mod = 10**9 + 7
+        
+        def getSum(n):
+            v = n*(n-1)
+            return v//2
+        
+        
+        c = Counter(d)
+        
+        powers = [2**i for i in range(0,25)]
+        st = set()
+        res = 0
+        
+        for i in c.keys():
+            if c[i]==0: continue
+                
+            freq = c[i]
+            
+            for power in powers:
+                if power - i in c:
+                    thatFreq = c[power - i]
+                    if thatFreq==0 or (power-i==i): 
+                        res+= getSum(freq)
+                        res%=mod
+                    else:
+                        key = (power-i,i)
+                        if not key in st:
+                            st.add((i,power-i))
+                            res += (freq*thatFreq)
+                            res%=mod
+                    
+                    # print(i,power-i)
+                    # c[i]-=m
+                    # c[power-i]-=m
+        # res//=2
+        
+        
+        # for i in c.keys():
+        #     freq = c[i]
+        #     for power in powers:
+        #         if power - i in c:
+        #             thatFreq = c[power - i]
+        #             if thatFreq==0 or (power-i==i): 
+        #                 res+= getSum(freq)
+        return res
