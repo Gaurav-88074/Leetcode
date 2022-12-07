1b2709238d1794aa0aa47823fb42616d1aa513bd
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-07 12:22:37 +0530

    Time: 2 ms (43.75%), Space: 67.2 MB (39.84%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int find(TreeNode root,int low,int high){
+        if(root==null){
+            return 0;
+        }
+        if(root.val==low){
+            return low;
+        }
+        else if(low<root.val){
+            int v = 0;
+            if(root.val<=high &&  root.val>=low){
+                v = root.val;
+            }
+            return v + find(root.left,low,high);
+        }
+        else{
+            int v = 0;
+            if(root.val<=high && root.val>=low){
+                v = root.val;
+            }
+            return v + find(root.right,low,high);
+        }
+        
+    }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null){
+            return 0;
+        }
+        
+        int left = rangeSumBST(root.left,low,high); 
+        int right = rangeSumBST(root.right,low,high); 
+        int v = 0;
+        if(root.val<=high&&root.val>=low){
+            v = root.val;
+        }
+        return left+right+v;
+        //------------------
+        // int left = find(root,low,high); 
+        // int right = find(root,low,high); 
+        // return left+right-root.val;
+    }
+}
