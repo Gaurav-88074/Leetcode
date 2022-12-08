f28506debc3bf79b2043553448679e7b5964a2ca
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-08 22:53:41 +0530

    Time: 64 ms (80.78%), Space: 14.6 MB (5.49%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,47 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        pivot=[None]
+        ans=[None]
+        def binarySearch(nums,low,high):
+            if low>high:
+                return False
+            mid = (low+high)//2
+            if nums[mid]==target:
+                return True
+            elif target<nums[mid]:
+                return binarySearch(nums,low,mid-1)
+            else:
+                return binarySearch(nums,mid+1,high)
+        def valueSearch(nums,target,low,high):
+            # global pivot
+            while low<=high:
+                # if low==high:
+                #     return nums[low]==target
+                mid=(low+high)//2
+                if mid-1>=0 and nums[mid]<nums[mid-1]:
+                    pivot[0]=mid
+                    print("mil gya")
+                    return True
+                if(nums[mid]==target):
+                    ans[0]=True
+                    return True
+                elif nums[mid]<nums[0]:
+                    high=mid-1
+                elif nums[mid]>nums[0]:
+                    low=mid+1
+                else:
+                    return valueSearch(nums,target,low,mid-1) or valueSearch(nums,target,mid+1,high)
+            return False
+        
+        pivotSearch = valueSearch(nums,target,0,len(nums)-1)
+        
+        print(pivotSearch,pivot)
+        
+        if ans[0]!=None:return ans[0]
+        
+        if pivot[0]!=None:
+            left = binarySearch(nums,0,pivot[0]-1)
+            right = binarySearch(nums,pivot[0],len(nums)-1)
+            return left or right
+        return target==nums[0]
+                
