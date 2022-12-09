080f3d85bdcdf3895a3be0a5337dc1fb55461e16
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-09 16:21:33 +0530

    Time: 1 ms (80.02%), Space: 43 MB (18.36%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -17,13 +17,16 @@ class Solution {
     int res = 0;
     public void go(TreeNode root,int minValue,int maxValue){
         if(root==null) return;
+        minValue = Math.min(minValue,root.val);
+        maxValue = Math.max(maxValue,root.val);
+        res = res > (maxValue-minValue) ? res : (maxValue-minValue);
+        go(root.left,minValue,maxValue);
+        go(root.right,minValue,maxValue);
+        // go(root.left,Math.min(root.val,minValue),Math.max(root.val,maxValue));
+        // res = res>Math.abs(root.val-minValue) ? res : Math.abs(root.val-minValue);
+        // res = res>Math.abs(root.val-maxValue) ? res : Math.abs(root.val-maxValue);
+        // go(root.right,Math.min(root.val,minValue),Math.max(root.val,maxValue));
         
-        go(root.left,Math.min(root.val,minValue),Math.max(root.val,maxValue));
-        
-        res = res>Math.abs(root.val-minValue) ? res : Math.abs(root.val-minValue);
-        res = res>Math.abs(root.val-maxValue) ? res : Math.abs(root.val-maxValue);
-        
-        go(root.right,Math.min(root.val,minValue),Math.max(root.val,maxValue));
     }
     public int maxAncestorDiff(TreeNode root) {
         go(root,root.val,root.val);
