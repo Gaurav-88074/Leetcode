bb0dbace005a9cea73b76d619288f631b2c822b5
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-13 14:34:31 +0530

    Time: 2626 ms (63.79%), Space: 55.5 MB (65.52%) - LeetHub

--- 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.py
+++ 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.py
@@ -1,34 +1,21 @@
-class Solution:
-    def maxStarSum(self, vals: List[int], edges: List[List[int]], k: int) -> int:
-        if k==0 or len(edges)==0:
-            q =max(vals)
-            # if q<0:
-            #     return 0
-            return q
-        graph = defaultdict(list)
-        node=set()
-        for a,b in edges:
-            node.add(a)
-            node.add(b)
-            if vals[b]>0:
-                graph[a].append(vals[b])
-            if vals[a]>0:
-                graph[b].append(vals[a])
-        res = float('-inf')
-        for i in graph:
-            graph[i].sort(reverse = True)
-        #     t = vals[i]
-        #     v = graph[i]
-        #     for j in range(0,min(k,len(v))):
-        #         t+=graph[i][j]
-        #     res = max(res,t)
+class Solution(object):
+    def maxStarSum(self, vals, edges, k):
+        n = len(vals)
+        maxres = max(vals)
+        g = defaultdict(list)
         
-        node = list(node)
-        for i in node:
-            arr = graph[i]
-            v = arr[0:k]
-            # t = vals+sum(v)
-            res = max(res,sum(v)+vals[i])
-            # res = max(res,sum(v))
-        return res
+        for a,b in edges:
+            g[a].append(vals[b])
+            g[b].append(vals[a])
+            
+        for i in range(n):
+            res = vals[i]
+            g[i].sort(reverse=True)
+            for x in g[i][:k]:
+                if x < 0:
+                    break
+                res += x
+            maxres = max(maxres, res)
+        
+        return maxres
         
