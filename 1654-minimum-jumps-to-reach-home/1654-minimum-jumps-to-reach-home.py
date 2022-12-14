252b19df99f0fb4c1deb48e8675e0abaca2c5d75
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-14 20:35:26 +0530

    Time: 497 ms (20.21%), Space: 109.5 MB (5.18%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        s = set(forbidden)
+        # sys.setrecursionlimit(10**5)
+        # @lru_cache(None)
+        dp = [[-1,-1] for i in range(6001)]
+        def compute(position,back):
+            
+            if position==x:
+                return 0
+            
+            if position<0:
+                return float('inf')
+            if position>6000:
+                return float('inf')
+            if back>=2:
+                return float('inf')
+            if position in s:
+                return float('inf')
+            
+            if dp[position][back]!=-1:
+                return dp[position][back]
+            
+            dp[position][back] = 1 + compute(position + a,0)
+            if not back:
+                dp[position][back] =  min(dp[position][back],1 + compute(position - b,back + 1))
+            return dp[position][back]
+        # left = compute(0,0)
+        # right= compute(0,0)
+        # print(left,right)
+        # res  = min(left,right)
+        res = compute(0,0)
+        return -1 if res == float('inf') else res
