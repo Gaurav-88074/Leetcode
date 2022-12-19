6200c7270de03a6d8dbd907c8af34dadd5b82b7e
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-19 19:29:47 +0530

    Time: 488 ms (82.05%), Space: 14.5 MB (40.06%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        true=0
+        false=0
+        index = 0
+        res = 0
+        # mn=0
+        for i,v in enumerate(answerKey):
+            if v=="T":
+                true+=1
+            else:
+                false+=1
+            mn = min(true,false)
+            while mn>k :
+                # print(mn,"min=")
+                ch = answerKey[index]
+                index+=1
+                if ch=="T":
+                    true-=1
+                else:
+                    false-=1
+                mn = min(true,false)
+            # print(answerKey[index:i+1])
+            res = max(res,i-index+1)
+        return res
