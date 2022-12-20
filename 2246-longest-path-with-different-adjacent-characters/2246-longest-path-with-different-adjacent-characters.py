61e83b8372b29fcd424d976f2bc00428671558d1
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-21 03:21:18 +0530

    Time: 3152 ms (71.17%), Space: 198.9 MB (5.18%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -0,0 +1,23 @@
+from sortedcontainers import SortedList,SortedDict
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        tree = defaultdict(list)
+        for i,v in enumerate(parent):
+            tree[v].append(i)
+        res = [0]
+        def dfs(node,res):
+            c = 0
+            l = SortedList()
+            for nxt in tree[node]:
+                value = dfs(nxt,res)
+                if s[node] != s[nxt]:
+                    c = max(c,1 + value)
+                    l.add(1 + value)
+            # print(tree[node],l)
+            if len(l)>=2:
+                res[0] = max(l[-1]+l[-2],res[0])
+            res[0]=max(c,res[0])
+            return c
+        
+        v = 1 + dfs(tree[-1][0],res)
+        return max(res[0]+1,v)
