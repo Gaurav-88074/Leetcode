2d92912630ecb98a9fdde73683a530a59af9259b
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-20 19:46:36 +0530

    Time: 5 ms (93.16%), Space: 46.4 MB (93.67%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
         // if(arr.length==1)return 1;
-        
+    
         int res = 0;
         int c = 0;
         int previous = 10;
