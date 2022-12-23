a537fe7289f71f075e4c690c29d6851339ca5811
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-23 06:54:05 +0530

    Time: 55 ms (77.95%), Space: 18.4 MB (25.99%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,16 +3,15 @@ class Solution:
     
         @lru_cache(None)
         def compute(index,buy):
-            if index>=len(prices):
-                return 0
-            
+            if index>=len(prices) : return 0
             take = 0
+            #--------------------------------------------------
             if buy==False:
                 take = - prices[index] + compute(index+1,True)
             else:
                 take =   prices[index] + compute(index+2,False)
-            
+            #---------------------------------------------------
             skip = compute(index+1,buy)
-            
             return max(take,skip)
+            #--------------------------------------------------
         return compute(0,False)
