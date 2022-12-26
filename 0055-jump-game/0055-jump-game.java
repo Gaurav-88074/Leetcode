2c6c78c72f2a839b5a0f043cdfc8c1953e7c3d10
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2022-12-26 06:19:04 +0530

    Time: 3 ms (77.19%), Space: 43 MB (81.90%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,14 +4,16 @@ class Solution {
         // if(nums.length==1) return true;
         int energy = nums[0];
         int index = 0;
-        while(energy>0 || nums[index]!=0){
+        while(energy >0 && index<nums.length-1){
+            energy-=1;
             energy = Math.max(energy,nums[index]);
             // System.out.println(energy+ " : "+index);
             index+=1;
-            energy-=1;
-            if(index>=nums.length){
-                return true;
-            }
+            // energy-=1;
+            // if(index>=nums.length){
+            //     return true;
+            // }
+            if(energy<=0)return false;
         }
         return index>=nums.length-1;
     }
