cbbb0321588f09b8542363eb1682eca8bfb42f2d
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-01-07 13:09:47 +0530

    Time: 1007 ms (54.19%), Space: 19.4 MB (23.25%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,20 +1,50 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         start = 0
-        end = 0 
-        sum = 0
-        length = 0
-        while start<len(gas) and length < len(gas):
-            sum += gas[end]-cost[end]
-            length +=1
-            while start<len(gas) and sum < 0:
-                sum -= gas[start] - cost[start]
-                start +=1
-                length-=1
-            end+=1
-            end = end % len(gas)
-        if length == len(gas):
-            return start
-        return -1
-
+        fuel = 0
+        # costamount =0
+        stationCovered = 0
+        currentPosition =0
         
+        problem = 0
+        visited = 0
+        while visited<2*len(gas)-1: 
+            visited+=1
+            # print("we are at ",currentPosition,"with",stationCovered,"station covered")
+            fuel = fuel + gas[currentPosition]
+            #---------------------------------
+            if stationCovered>=len(gas) : 
+                return start 
+            #----------------------------------
+            while fuel-cost[currentPosition] <0 and start<currentPosition:
+                 
+                # print("problem occured at",currentPosition,"fuel=",fuel,\
+                # "cost=",cost[currentPosition],"s=",start)
+                 
+                problem +=1
+                 
+                fuel-=abs(gas[start] - cost[start]) 
+                
+                # print("fuel at",currentPosition,fuel)
+                
+                start+=1 
+                 
+                stationCovered -= 1 
+            #---------------------------------------------
+            if fuel-cost[currentPosition]<0: 
+                # print("new problem occured at",currentPosition,"fuel=",fuel,\
+                # "cost=",cost[currentPosition],"s=",start)
+                fuel = 0 
+                problem+=1
+                stationCovered = 0 
+                start+=1
+            else: 
+                stationCovered+=1
+                fuel = fuel - cost[currentPosition]
+            #---------------------------------------
+            currentPosition+=1
+            currentPosition%=len(gas)
+            #---------------------------------------
+            if stationCovered>=len(gas) : 
+                return start 
+        return -1
