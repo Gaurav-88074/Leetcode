e5839f18bb2c6b1d4ccdb1c327570402bd5d30f9
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-01-07 13:03:47 +0530

    Time: 1087 ms (52.14%), Space: 19.4 MB (23.25%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,50 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        fuel = 0
+        # costamount =0
+        stationCovered = 0
+        currentPosition =0
+        
+        problem = 0
+        visited = 0
+        while visited<2*len(gas)-1: 
+            visited+=1
+            # print("we are at ",currentPosition,"with",stationCovered,"station covered")
+            fuel = fuel + gas[currentPosition]
+            #---------------------------------
+            if stationCovered>=len(gas) : 
+                return start 
+            #----------------------------------
+            while fuel-cost[currentPosition] <0 and start<currentPosition:
+                 
+                # print("problem occured at",currentPosition,"fuel=",fuel,\
+                # "cost=",cost[currentPosition],"s=",start)
+                 
+                problem +=1
+                 
+                fuel-=abs(gas[start] - cost[start]) 
+                
+                # print("fuel at",currentPosition,fuel)
+                
+                start+=1 
+                 
+                stationCovered -= 1 
+            #---------------------------------------------
+            if fuel-cost[currentPosition]<0: 
+                # print("new problem occured at",currentPosition,"fuel=",fuel,\
+                # "cost=",cost[currentPosition],"s=",start)
+                fuel = 0 
+                problem+=1
+                stationCovered = 0 
+                start+=1
+            else: 
+                stationCovered+=1
+                fuel = fuel - cost[currentPosition]
+            #---------------------------------------
+            currentPosition+=1
+            currentPosition%=len(gas)
+            #---------------------------------------
+            if stationCovered>=len(gas) : 
+                return start 
+        return -1
