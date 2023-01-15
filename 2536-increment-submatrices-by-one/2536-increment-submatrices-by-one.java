d0c053fb4d15a82b4ef0a5408d930e454e505c72
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-01-15 17:57:44 +0530

    Time: 21 ms (100.00%), Space: 50.4 MB (100.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,14 +1,24 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
         int[][] mat = new int[n][n];
+        //brute force
         for (int[] query : queries) {
-            int row1 = query[0], col1 = query[1], row2 = query[2], col2 = query[3];
-            for (int i = row1; i <= row2; i++) {
-                for (int j = col1; j <= col2; j++) {
-                    mat[i][j]++;
-                }
+            int x1 = query[0], y1 = query[1], x2 = query[2], y2 = query[3];
+            for (int i = x1; i <= x2; i++) {
+                mat[i][y1]+=1;
+                
+                if (y2+1<n) mat[i][y2+1]-=1;
+            }   
+        }
+        for(int i = 0; i < n; i++){
+            int v=0;
+            for(int j = 0; j < n; j++){
+                
+                v+=mat[i][j];
+                mat[i][j]=v;
             }
         }
+            
         return mat;
     }
 }
