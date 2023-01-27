6a070c0199ee520ad58e70a9e0e5561f9e461c0a
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-01-27 17:20:54 +0530

    Time: 652 ms (45.50%), Space: 27.6 MB (47.98%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        s = set(words)
+        
+        def is_possible(word,index,char,count):
+            if index==len(word):
+                if char in s and count>0:
+                    return True
+                return False
+            char+=word[index]
+            if char in s: 
+                if word[index+1: ] in s or is_possible(word,index+1,"",count+1):
+                    return True
+            return is_possible(word,index+1,char,count)
+        res = []
+        for i in words:
+            if is_possible(i,0,"",0):
+                res.append(i)
+        return res
