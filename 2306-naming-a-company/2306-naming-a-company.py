20c1ae2adfe23a94f4d93c155c6df5dd0eba5597
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-02-09 13:33:05 +0530

    Time: 635 ms (83.69%), Space: 28.5 MB (58.70%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -4,9 +4,10 @@ class Solution:
         for a in ideas:
             count[a[0]].add((a[1:]))
         res = 0
+        # print(count.items())
         for a, seta in count.items():
             for b, setb in count.items():
-                if a >= b: continue
+                if a == b: continue
                 same = len(seta & setb)
                 res += (len(seta) - same) * (len(setb) - same)
-        return res * 2
+        return res 
