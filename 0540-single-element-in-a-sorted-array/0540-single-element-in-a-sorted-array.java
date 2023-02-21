ddbe5f56bb96920ebfb9f279c95021d75a77efa2
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-02-21 16:49:11 +0530

    Time: 0 ms (100.00%), Space: 48 MB (72.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,8 +1,35 @@
-class Solution(object):
-    def compute(self,nums,i):
-        if i==len(nums):
-            return 0
-        return nums[i]^self.compute(nums,i+1)
-    def singleNonDuplicate(self, nums):
-        return self.compute(nums,0)
-        
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        // [1,1,2,3,3,4,4,8,8]
+        // [0,1,2,3,4,5,6,7,8]
+        int low = 0;
+        int high=nums.length - 1;
+        int mid;
+        int elements;
+        while(low<=high){
+            mid = low + ((high-low)/2);
+            elements = mid + 1;
+            if(elements%2==0){
+                if(mid-1>=0 && nums[mid-1]!=nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low  = mid + 1;
+                }
+            }
+            else{
+                // System.out.println("odd case hit "+mid);
+                if(mid-1>=0 && nums[mid-1]==nums[mid]){
+                    high = mid - 1;
+                    // System.out.println("Going towards left "+low+" "+high);
+                }
+                else{
+                    low  = mid + 1;
+                    // System.out.println("Going towards right "+low+" "+high);
+                }
+            }
+        }
+        // System.out.println(low+" "+high);
+        return nums[high];
+    }
+}
