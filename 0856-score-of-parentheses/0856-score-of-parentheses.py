860b9d8171273e7f7e8311e2e2cd89294e18e06b
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-04-05 14:55:46 +0530

    Time: 34 ms (43.87%), Space: 13.9 MB (48.84%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -2,11 +2,9 @@ class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stack = []
         res = 0
-        temp=0
         for i,c in enumerate(s):
             if c=='(':
-                score = 0
-                stack.append([i,c,score])
+                stack.append([i,c,0])
             else:
                 if stack and stack[-1][1]=='(':
                     node = stack.pop()
@@ -19,7 +17,6 @@ class Solution:
                             res+=1
                     else:
                         value = node[2]
-                        # res-=value
                         
                         if stack:
                             stack[-1][2] += 2*value
