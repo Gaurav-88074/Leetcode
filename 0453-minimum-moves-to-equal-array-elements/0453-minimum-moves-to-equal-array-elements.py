b056dc84ebea7c966e619f1f36c35f5b4726db88
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-04-11 09:02:53 +0530

    Time: 257 ms (53.72%), Space: 16 MB (24.68%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
@@ -2,16 +2,7 @@ from sortedcontainers import SortedList
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
         res = 0
-        # l  = SortedList(nums,key = lambda x : -x)
-        
-        # while l[0]!=l[-1]:
-        #     mn = l[-1]
-        #     l.remove(mn)
-        #     mn-=1
-        #     l.add(mn)
-        #     res+=1
-        # return res
-
+        # do dec instead of inc
         mn = min(nums)
         for i in nums:
             res+=abs(i-mn)
