7dba5d969e7bae1628da25840057b0595e4ce967
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-05-16 01:20:26 +0530

    Time: 1366 ms (26.61%), Space: 91.2 MB (62.10%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -1,39 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    private ListNode thatNode;
-    private int maxSize;
-    public void compute(ListNode head,int size,int k){
-        if(head==null){
-            maxSize = size;
-            return;
-        }
-        if(size==k){
-            thatNode = head;
-        }
-        compute(head.next,size+1,k);
-        if(maxSize-size==k){
-            int temp = head.val;
-            head.val = thatNode.val;
-            thatNode.val = temp;
-        }
-        //-------------------------------
-        
-    }
-//     public ListNode fast(ListNode head, int k){
-        
-    // }
-    public ListNode swapNodes(ListNode head, int k) {
-        //it worked
-        compute(head,1,k);
-        return head;
-    }
-}
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapNodes(self, head, k):
+        d = [defaultdict(None)]
+        temp = head
+        i=1
+        while temp!=None:
+            d.append(temp)
+            i+=1
+            temp=temp.next
+        d[k].val ,d[-k].val= d[-k].val ,d[k].val
+        return head
