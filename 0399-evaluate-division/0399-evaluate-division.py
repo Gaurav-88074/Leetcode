01111be092ec09b3b549c91c7276f7d1e961e470
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-05-20 07:31:20 +0530

    Time: 48 ms (23.02%), Space: 16.5 MB (8.30%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,33 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        res = []
+        d = defaultdict(int)
+        graph = defaultdict(list)
+        i = 0
+        for a,b in equations:
+            d[(a,b)]=values[i]
+            d[(b,a)]=1/values[i]
+            i+=1
+            graph[a].append(b)
+            graph[b].append(a)
+        # print(d)
+        # print(graph)
+        def compute(node,dest,prev,v):
+            if node in v:
+                return float('inf')
+            if node==dest and node in graph:
+                return 1
+            v.add(node)
+            res = []
+            for nxt in graph[node]:
+                res.append(d[(node,nxt)] * compute(nxt,dest,prev,v))   
+            if res:return min(res)
+            return float('inf')
+        for a,b in queries:
+            val = compute(a,b,None,set())
+            if val==float('inf'):
+                res.append(-1)
+            else:
+                res.append(val)
+        return res
+            
