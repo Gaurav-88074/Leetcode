0c0b1ea5589536d32150907dbd232511e7b84b05
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-05-24 18:06:27 +0530

    Time: 1398 ms (7.25%), Space: 39.9 MB (14.50%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -0,0 +1,24 @@
+from sortedcontainers import SortedList, SortedDict,SortedSet
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        
+        # if nums1==[2,1,14,12] and nums2 == [11,7,13,6] :return 168
+        arr = []
+        for i in range(len(nums1)):
+            arr.append([
+                nums1[i],nums2[i]
+            ])
+        arr.sort(key = lambda x :  (-x[1]))
+        res = 0
+        # print(arr)
+        value = 0
+        minheap = SortedList()
+        for i in range(len(arr)):
+            a,b = arr[i]
+            value+=a
+            minheap.add(a)
+            if i>=(k-1):
+                res = max(res ,value * b)
+                value-= minheap.pop(0)
+                  
+        return res
