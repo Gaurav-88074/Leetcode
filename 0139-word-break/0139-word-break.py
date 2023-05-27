bd46d472fcd7173bd112af136f72a06864ef9b1e
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-05-27 23:13:44 +0530

    Time: 68 ms (5.74%), Space: 16.7 MB (5.16%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        d = set(wordDict)
+
+        @lru_cache(None)
+        def compute(x):
+            if x == len(s):
+                return True
+            take = False
+            # skip = 1 + compute(x + 1)
+
+            for i in range(x, len(s)):
+                if s[x:i+1] in d:
+                    take = take or compute(i + 1)
+                    # skip = min(skip, take)
+
+            return take
+
+        return compute(0)
