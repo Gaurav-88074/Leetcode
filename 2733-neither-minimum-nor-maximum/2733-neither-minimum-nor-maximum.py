c5c4da314c3c82a7d26e966109dedbf264c54299
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-06-12 14:27:08 +0530

    Time: 346 ms (100.00%), Space: 13.4 MB (100.00%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -1,27 +1,17 @@
 class Solution(object):
     def findNonMinOrMax(self, nums):
-#         minimum = maximum = nums[0]
 
-#         for num in nums[1:]:
-#             minimum = min(minimum, num)
-#             maximum = max(maximum, num)
-
-#         for num in nums:
-#             if num != minimum and num != maximum:
-#                 return num
-
-#         return -1
         if len(nums)<=2:
             return -1
-        # nums.sort()
-        # return nums[1]
+        nums.sort()
+        return nums[1]
         
-        mn = float('inf')
-        mx = float('-inf')
-        for i in nums:
-            mn = min(mn,i)
-            mx = max(mx,i)
-        nums.remove(mn)
-        nums.remove(mx)
-        return nums[0]
+        # mn = float('inf')
+        # mx = float('-inf')
+        # for i in nums:
+        #     mn = min(mn,i)
+        #     mx = max(mx,i)
+        # nums.remove(mn)
+        # nums.remove(mx)
+        # return nums[0]
         
