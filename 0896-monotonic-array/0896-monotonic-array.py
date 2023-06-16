885dbc5d17e3999e56ca0ab7792856bb0e462530
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-06-16 14:14:03 +0530

    Time: 931 ms (87.89%), Space: 31 MB (35.85%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -3,9 +3,9 @@ class Solution:
         inc = True
         dec = True
         for i in range(1,len(nums)):
-            if nums[i]<nums[i-1]:
+            if inc and nums[i]<nums[i-1]:
                 inc = False
-            if nums[i]>nums[i-1]:
+            if dec and nums[i]>nums[i-1]:
                 dec = False
             
         return inc or dec
