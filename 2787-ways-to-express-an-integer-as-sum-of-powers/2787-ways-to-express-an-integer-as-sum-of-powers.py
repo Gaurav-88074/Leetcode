634e243f0e4bf1595a0c5963992fc2de39dc166b
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-07-23 20:34:48 +0530

    Time: 142 ms (100.00%), Space: 23.5 MB (60.00%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.py
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,23 +1,31 @@
 import math
 class Solution:
+    dp = {}
     def numberOfWays(self, n: int, x: int) -> int:
         mod = 10**9 + 7
         till = int(pow(n,1/x))+1
         powermap = {}
         for i in range(1,till+2):
             powermap[i]=pow(i,x,mod)
-        @lru_cache(None)
-        def compute(n,i):
+        # @lru_cache(None)
+        def compute(n,x,i):
+            key = (n,x,i)
+            
             if n==0:
                 return 1
             
             if n<0 or n-powermap[i]<0:
                 return 0
-            
-            take = compute(n - powermap[i] , i+1)%mod
-            skip = compute(n,i+1)%mod
+            #--------------------------
+            if key in Solution.dp:
+                return Solution.dp[key]
+            #-----------------------------
+            take = compute(n - powermap[i] ,x, i+1)%mod
+            skip = compute(n,x,i+1)%mod
             
             res = (take+skip)%mod
             
+            Solution.dp[key]=res
+            
             return res
-        return compute(n,1)%mod
+        return compute(n,x,1)%mod
