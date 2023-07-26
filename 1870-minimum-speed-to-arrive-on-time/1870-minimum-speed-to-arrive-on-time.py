41bb08655b654b0a149a4a206cd8ff64a6ee0243
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-07-26 14:40:43 +0530

    Time: 3780 ms (33.90%), Space: 31 MB (9.40%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        left = 1
+        right = 10**7+1
+        
+        def computeSpeed(assumeSpeed):
+            res = 0
+            n = len(dist)
+            for i in range(0,n-1):
+                val = math.ceil(dist[i]/assumeSpeed)
+                res+=val
+            res+=dist[-1]/assumeSpeed
+            return res
+        res = -1
+        while left<right:
+            mid= (left+right)//2
+            computedHours = computeSpeed(mid)
+            # print("Assume Speed =",mid,"Computed Hours =",computedHours)
+            if computedHours<=hour:
+                res = mid
+                right = mid
+            else:
+                left = mid+1
+            # print(left,right,"last response")
+        return res
