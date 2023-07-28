dacae825bf4d51203e629a74b05bb9ec0875c379
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-07-28 22:51:09 +0530

    Time: 52 ms (66.38%), Space: 16.6 MB (19.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -10,15 +10,18 @@ class Solution:
                 secondPersonChoice2 = compute(i,j-1,size-1 ,False)
                 # print(f"state i={i} j={j} o_choice1 ={secondPersonChoice1}\
                 # o_choice2={secondPersonChoice2}")
+                #---------------------------
+                #we want him to pick worst
+                #--------------------------
                 return min(secondPersonChoice1,secondPersonChoice2)
             else:
                 choice1 = nums[i] + compute(i+1,j,size-1 ,True)
                 choice2 = nums[j] + compute(i,j-1,size-1 ,True)
                 # print(f"state i={i} j={j} choice1 ={choice1}  choice2={choice2}")
+                #---------------------------
+                #we want him to pick best
+                #--------------------------
                 return max(choice1,choice2)
-            #-------------------------------------------
-            # we are taking min because we are assuming he got worst luck
-            #-------------------------------------------
         n =len(nums)
         res = compute(0,n-1,n,False)
         # print(res)
