7c1e09412ce00e94063cda911e84831d47880d3d
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-13 07:58:20 +0530

    Time: 971 ms (75.39%), Space: 125.3 MB (13.08%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        def op1(nums,index):
+            if index+1>=len(nums) : return False
+            return nums[index]==nums[index+1]
+        def op2(nums,index):
+            if index+2>=len(nums) : return False
+            return nums[index]==nums[index+1]==nums[index+2]
+        def op3(nums,index):
+            if index+2>=len(nums) : return False
+            return nums[index+2]-nums[index+1]==1 and nums[index+1] - nums[index]==1
+        @lru_cache(None)
+        def check(index):
+            if index==len(nums):
+                return True
+            if op1(nums,index) and check(index+2):
+                return True
+            if op2(nums,index) and check(index+3):
+                return True
+                 
+            if op3(nums,index) and check(index+3):
+                return True
+            
+            return False
+        
+        # print(right)
+        return check(0)
