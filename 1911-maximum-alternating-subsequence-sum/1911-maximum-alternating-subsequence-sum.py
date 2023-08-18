d952564f137c45a79e345b4caf878e82345dd963
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-18 22:44:57 +0530

    Time: 1890 ms (40.46%), Space: 203.8 MB (39.23%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        
+        @lru_cache(None)
+        def compute(index,lastWasEven):
+            if index==len(nums):
+                return 0
+            if lastWasEven :
+                #why - : if last was even then this is odd
+                #so we have to subtract it now
+                take = -nums[index] + compute(index+1,False)
+                skip = compute(index+1,lastWasEven)
+                return max(take,skip)
+            else:
+                take = nums[index] + compute(index+1,True)
+                skip = compute(index+1,lastWasEven)
+                return max(take,skip)
+        
+        res = compute(0,False)
+        compute.cache_clear()
+        return res
