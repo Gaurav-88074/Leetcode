b2be2c4126b7f637f5498912e5105cd37e19cf1e
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-20 23:36:39 +0530

    Time: 710 ms (8.44%), Space: 20.6 MB (78.31%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -1,22 +1,20 @@
-class Solution(object):
-    def minimumXORSum(self, nums1, nums2):
+class Solution:
+    def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         def isUsed(mask,i):
             return (mask & (1<<i))>0
-        dp ={}
-        def compute(i,mask_a,mask_b):
+        @lru_cache(None)
+        def compute(mask_a,mask_b):
             if mask_a== (1<<n)-1:
                 return 0
-            key = (i,mask_a,mask_b)
-            if key in dp : return dp[key]
+            i = bin(mask_a)[2:].count('1')
             res = float('inf')
             v = nums1[i]
             for j,w in enumerate(nums2):
                 if not isUsed(mask_a,i) and not isUsed(mask_b,j):
-                    temp = (v^w) + compute(i+1,mask_a|(1<<i) , mask_b|(1<<j))
+                    temp = (v^w) + compute(mask_a|(1<<i) , mask_b|(1<<j))
                     res =min(temp,res)
-            dp[key] = res
             return res
-        res = compute(0,0,0)
+        res = compute(0,0)
+        compute.cache_clear()
         return res
-        
