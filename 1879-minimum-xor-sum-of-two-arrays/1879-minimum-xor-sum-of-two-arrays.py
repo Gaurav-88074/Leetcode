213a917a33b3fc8561edcfbda498fd8c16ad8505
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-20 23:31:58 +0530

    Time: 672 ms (10.84%), Space: 20.5 MB (78.31%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -1,38 +1,19 @@
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
-        # n = len(nums1)
-        # def isUsed(mask,i):
-        #     return (mask & (1<<i))>0
-        # @lru_cache(None)
-        # def compute(i,mask_a,mask_b):
-        #     if mask_a== (1<<n)-1:
-        #         return 0
-        #     res = float('inf')
-        #     v = nums1[i]
-        #     for j,w in enumerate(nums2):
-        #         if not isUsed(mask_a,i) and not isUsed(mask_b,j):
-        #             temp = (v^w) + compute(i+1,mask_a|(1<<i) , mask_b|(1<<j))
-        #             res =min(temp,res)
-        #     return res
-        # res = compute(0,0,0)
-        # compute.cache_clear()
-        # return res
         n = len(nums1)
         def isUsed(mask,i):
             return (mask & (1<<i))>0
-        dp ={}
+        @lru_cache(None)
         def compute(i,mask_a,mask_b):
             if mask_a== (1<<n)-1:
                 return 0
-            key = (i,mask_a,mask_b)
-            if key in dp : return dp[key]
             res = float('inf')
             v = nums1[i]
             for j,w in enumerate(nums2):
                 if not isUsed(mask_a,i) and not isUsed(mask_b,j):
                     temp = (v^w) + compute(i+1,mask_a|(1<<i) , mask_b|(1<<j))
                     res =min(temp,res)
-            dp[key] = res
             return res
         res = compute(0,0,0)
+        compute.cache_clear()
         return res
