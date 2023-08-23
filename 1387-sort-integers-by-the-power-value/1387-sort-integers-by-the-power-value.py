7d2d40902ae2057315aa6d015627fba27ec5fe80
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-23 13:06:51 +0530

    Time: 141 ms (91.51%), Space: 23.6 MB (32.60%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        
+        @lru_cache(None)
+        def compute(n):
+            if n==1:
+                return 0
+            if (n&1)==1:
+                return 1 + compute(3*n+1)
+            else:
+                return 1 + compute(n//2)
+        nums = []
+        for i in range(lo,hi+1):
+            nums.append(i)
+        nums.sort(key = lambda x : compute(x))
+        # print(nums)
+        return nums[k-1]
