4fa4da21a03e1e7c092537d8cf5ce32bd2ec4101
Gaurav <81903385+Gaurav-88074@users.noreply.github.com>
2023-08-25 22:41:14 +0530

    Time: 10 ms (88.04%), Space: 13.2 MB (80.98%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,19 +1,4 @@
-class Solution:
-    def divisorGame(self, n: int) -> bool:
-        #P_id
-        #1 -> Alice
-        #0 -> BOB
-        
-#         def compute(n,p):
-            
-#             if n==1:
-#                 return p==0
-            
-#             res = False
-#             for x in range(1,n):
-#                 if n%x==0:
-#                     res = res or  compute(n-x,1-p)
-#             return res
-#         return compute(n,1)
-        
+class Solution(object):
+    def divisorGame(self, n):
         return (n&1)==0
+        
